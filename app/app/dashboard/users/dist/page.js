d2850de34de8d190d34241b9ae16ade1bce1e9b5
Juli <julianalicode@gmail.com>
2024-06-11 13:12:24 -0400

    delete dist js files

--- app/app/dashboard/users/dist/page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-function Users() {
-    return react_1["default"].createElement("div", null, "Users");
-}
-exports["default"] = Users;
